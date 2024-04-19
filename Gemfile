9287dea152ad6fae3fb817b2d75baf2e8a5c6a44
Dinshaw Gobhai <gobhai@gmail.com>
2024-04-19 16:01:31 -0600

    First commit.

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source "https://rubygems.org"
+
+gem "rubocop", "~> 1.62", require: false
+  gem "rubocop-minitest", "~> 0.35", require: false
---
+++ my_test_class.rb
@@ -0,0 +1,5 @@
+class MyTestClass
+  def foo
+    [ :client, :bot_pusher, :token_fetcher, :user_id_fetcher, :conversation_fetcher, :salutation ]
+  end
+end

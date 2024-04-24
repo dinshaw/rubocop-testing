56557ef8c16df3391586ab488691ef52b54555d5
Dinshaw Gobhai <gobhai@gmail.com>
2024-04-24 12:05:37 -0600

    More cops

--- my_test_class.rb
+++ my_test_class.rb
@@ -2,4 +2,33 @@ class MyTestClass
   def foo
     [ :client, :bot_pusher, :token_fetcher, :user_id_fetcher, :conversation_fetcher, :salutation ]
   end
+
+  def bar
+    redirect_to(
+      [
+        "https://login.microsoftonline.com/common/adminconsent?",
+        "some string"
+      ].join(""),
+      allow_other_host: true
+    )
+  end
+
+  def foobar
+    some_block do
+      another_block do
+        redirect_to(
+          [
+            "https://login.microsoftonline.com/common/adminconsent?",
+            "some string"
+          ].join(""),
+          allow_other_host: true
+        )
+      end
+    end
+  end
+
+  private
+    def redirect_to(url, allow_other_host: false)
+      puts url
+    end
 end
